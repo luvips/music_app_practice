e4dac68887c267bcff88e911cc0f6578c63dc041
luvips <luviaamagaali07@gmail.com>
2025-09-30 18:51:46 +0000

    modificacion en clase

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-import createPlaylist from "./utils/playlist.js";
-
-let progress_bar = document.getElementById("progress");
-let media = document.getElementById("media");
-let play_btn = document.getElementById("play");
-const song_img = document.getElementById("song-img");
-const lastest = document.getElementById("lastest");
-const forward = document.getElementById("forward")
+import player from "./app/player.js";
 
 const songs = [
         {
@@ -28,62 +21,10 @@ const songs = [
         },
     ];
 
-const last = [];
-
-const playlist = createPlaylist(songs.length);
-
-let playingNow;
-
 window.addEventListener('DOMContentLoaded', () => {
-    playingNow = playlist.pop()
-    loadSong(playingNow);
+    player.initializePlayer(songs);
+    
 })
-
-function loadSong(i){
-    const title = document.getElementById("title");
-    const artist = document.getElementById("artist");
-
-    const now = songs[i];
-
-    media.src = now.song_url;
-    title.innerText = now.song_name;
-    artist.innerText = now.artist_name;
-    song_img.src = now.caratula;
-}
-
-media.addEventListener('loadedmetadata', () => {
-    progress_bar.max = 100;
-    progress_bar.value = 0;
-    if(play_btn.classList.contains("pause")){
-        media.play();
-    }
-});
-
-media.ontimeupdate = function updateProgressBar() {
-    const progress_value = (this.currentTime / this.duration) * 100;
-    progress_bar.value = progress_value;
-}
-
-lastest.addEventListener('click', function(){
-    if(!last.length == 0){
-        playlist.push(playingNow);
-        playingNow = last.pop();
-        loadSong(playingNow)
-    }
-});
-forward.addEventListener('click', function(){
-    if(!playlist.length == 0){
-        last.push(playingNow);
-        playingNow = playlist.pop();
-        loadSong(playingNow)
-    }
-});
-
-progress_bar.oninput = function() {
-    media.currentTime = (this.value/100) * media.duration;
-}
-
-play_btn.addEventListener("click", playPause);
 
 function playPause(){
     if(play_btn.classList.contains("pause")){
@@ -98,4 +39,3 @@ function playPause(){
         play_btn.innerText = "Pause";
     }
 }
-
